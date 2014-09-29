77143395c394cf2341a55247639c37881f27e14a
Shelly Jindal <jinshelly@gmail.com>
2014-09-29 13:41:45 -0700

    for dashboard calculations

--- php/dashboardCalculations.php
+++ php/dashboardCalculations.php
@@ -7,92 +7,36 @@
 	$action=$_GET['action'];
 
 	if($action=='youOwed'){
-
-				$dbConn=connectToDb();
-				$doublearray = array();
-				$personNAME="";
-				$total_amount=0;
-				$youOweQuery = "select * from MoneyDistribution where Person_who_needs_To_ReturnMoney='$userName'";
-				$youOweQueryResult = mysqli_query($dbConn,$youOweQuery) or trigger_error("Query Failed! SQL: $conn - Error: ".mysqli_error(), E_USER_ERROR);
+				$dbConn=connectToDb();										
+				$youOweQuery="select a.amir , a.total , a.garib, b.firstname , b.lastname from (select Person_who_paid_the_Money as amir ,sum(amount_to_be_Returned) as 
+							  total ,Person_who_needs_To_ReturnMoney as garib from MoneyDistribution where Person_who_needs_To_ReturnMoney = '$userName' group by 
+							  Person_who_needs_To_ReturnMoney) a INNER JOIN (Select FirstName as firstname ,LastName as lastname ,Email as em from jinshelly_signup)b  ON b.em = a.garib";	
+			    $youOweQueryResult = mysqli_query($dbConn,$youOweQuery) or trigger_error("Query Failed! SQL: $conn - Error: ".mysqli_error(), E_USER_ERROR);
 				$checkrows=mysqli_num_rows($youOweQueryResult);
-				$count = 0;
-					if($checkrows==0){
+				if($checkrows==0){
 						echo "<br/>"."You do not owe any money.";						
-					}else{
+				}else{	
 						while ($row = mysqli_fetch_array($youOweQueryResult)){
-							       
-							        $doublearray[$count] = array();
-									$amount=$row['amount_to_be_Returned'];
-									$doublearray[$count]['amount'] = $amount;
-									$personWhoOwed = $row['Person_who_Paid_the_Money'];
-									$nameQuery = "select * from jinshelly_signup where Email='$personWhoOwed'";
-									$nameQueryResult = mysqli_query($dbConn,$nameQuery);
-									if(mysqli_num_rows($nameQueryResult)>0){
-					           						while ($rowName = mysqli_fetch_array($nameQueryResult)){
-					           								$personNAME = $rowName['FirstName']." ".$rowName['LastName'];
-					           								$doublearray[$count]['name'] = $personNAME;	
-
-					           						}
-					           		}
-					           		$count = $count+1;
-
+							echo "<br/>".'You Owe '.$row['firstname']." ".$row['lastname'].' $ '.$row['total'];
 						}
-						$calculatedArray = computeOutcome($doublearray);
-                        foreach ($calculatedArray as $key => $value) {
-                        		echo "<br/>".'You Owe '.$key.' $ '.$value.'<br/>';
-                        }
-					}						
+				}				
 	}
 	else{// for action youAreOwed
 				$db=connectToDb();
-				$doublearray = array();
-				$personNAME="";
-				$total_amount=0;
-				$count = 0;
-				$sql = "select * from MoneyDistribution where Person_who_Paid_the_Money='$userName'";
+				$sql="select a.amir , a.total , a.garib, b.firstname , b.lastname from (select Person_who_paid_the_Money as amir ,sum(amount_to_be_Returned) as total ,      
+					 	Person_who_needs_To_ReturnMoney as garib from MoneyDistribution where Person_who_Paid_the_Money = '$userName' group by Person_who_needs_To_ReturnMoney)
+					 	a INNER JOIN (Select FirstName as firstname ,LastName as lastname ,Email as em from jinshelly_signup)b  ON b.em = a.garib";
 				$sqlResult = mysqli_query($db,$sql);
 				$checkrows=mysqli_num_rows($sqlResult);
 				if($checkrows==0){
 						echo "<br/>"."You are not owed any money.";
 				}else{
-			           while ($row = mysqli_fetch_array($sqlResult)){
+						while ($row = mysqli_fetch_array($sqlResult)){
+							echo "<br/>".$row['firstname']." ".$row['lastname']. " owes you ".'$ '.$row['total'];
 
-			           		$doublearray[$count] = array();
-			           		$amount=$row['amount_to_be_Returned'];
-			           		$doublearray[$count]['amount'] = $amount;
-			           		$personWhoOwed = $row['Person_who_needs_To_ReturnMoney'];
-			           		$nameQuery = "select * from jinshelly_signup where Email='$personWhoOwed'";
-			           		$nameQueryResult = mysqli_query($db,$nameQuery);
-			           		if(mysqli_num_rows($nameQueryResult)>0){
-			           						while ($rowName = mysqli_fetch_array($nameQueryResult)){
-			           								$personNAME = $rowName['FirstName']." ".$rowName['LastName'];
-			           								$doublearray[$count]['name'] = $personNAME;
-			           						}
-			           		}
-			           		$count = $count+1;
-			           }
-                        $calculatedArray = computeOutcome($doublearray);
-                        foreach ($calculatedArray as $key => $value) {
-                        		echo "<br/>".$key. " owes you ".'$ '.$value;
-                        }
-
+						}
 				}
 	}
-
-
-	function computeOutcome($array) {
-												    $result=array();
-												    foreach ($array as $item) {
-												        $id=$item['name'];
-												        if (isset($result[$id])) {
-												            $result[$id]=$result[$id]+$item['amount'];
-												        } else {
-												            $result[$id]=$item['amount'];
-												        }
-												    }
-												    return $result;
-						}
-
 
 
 ?>
--- php/updateQuery.php
+++ php/updateQuery.php
@@ -1,34 +1,34 @@
 <?php
 
 
-$id = $_GET['id'];
-$val = $_GET['edit'];
+                            $id = $_GET['id'];
+                            $val = $_GET['edit'];
 
-echo "\n $val";
-updateData($val,$id);
-?>
+                            echo "\n $val";
+                            updateData($val,$id);
+                            ?>
 
 
-<?php
+                            <?php
 
-function updateData($edited_text,$id) {
-    $sql = "UPDATE jinshelly_signup SET $id='$edited_text' WHERE Email ='jinshelly@gmail.com'";
-    try {
-        $db = connectToDb();
-        mysqli_query($db,$sql);
+                            function updateData($edited_text,$id) {
+                                $sql = "UPDATE jinshelly_signup SET $id='$edited_text' WHERE Email ='jinshelly@gmail.com'";
+                                try {
+                                    $db = connectToDb();
+                                    mysqli_query($db,$sql);
 
-    	} catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
+                                	} catch(PDOException $e) {
+                                    echo '{"error":{"text":'. $e->getMessage() .'}}';
+                                }
+                            }
 
-function connectToDb(){
+                            function connectToDb(){
 
-				       	$con=mysqli_connect("localhost","db1","shellybelly10","my_db_shelly");
-						if (mysqli_connect_errno()) {
-							echo "<br/>";
-							echo "DB Connection Failed";
-						}
-						return $con;
-}
+                            				       	$con=mysqli_connect("localhost","db1","shellybelly10","my_db_shelly");
+                            						if (mysqli_connect_errno()) {
+                            							echo "<br/>";
+                            							echo "DB Connection Failed";
+                            						}
+                            						return $con;
+                            }
 ?>
